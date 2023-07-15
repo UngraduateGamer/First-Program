3605bf7fec827d15ee4abdfba75f507da1e6eab4
UngraduateGamer <139570725+UngraduateGamer@users.noreply.github.com>
2023-07-15 12:36:04 +0530

    Create first program.py

---
+++ first program.py
@@ -0,0 +1,13 @@
+# All are valid 
+
+# Used for Single line but print the double quotes 
+print('"Rahul"')
+
+# Used for print the multiple lines eg.stories
+print('''Twinkle Twinkle
+little star 
+how are wonder what you are..''')
+
+#Simple single line print usuage
+print("Rahul")
+print(5+10)
